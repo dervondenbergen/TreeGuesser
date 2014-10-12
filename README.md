ef17f40ac12e8b431ebf8b6164ebc4050718a929
Felix De Montis <felix@demont.is>
2014-10-12 20:54:05 +0200

    Erster Commit - vieles funktioniert nicht und die Baumdaten sind zur Zeit nur für um das Schloss Laudon

---
+++ README.md
@@ -0,0 +1,5 @@
+#TreeGuesser
+
+Errate das Alter eines Baumes in deiner Nähe!
+
+Ein Projekt welches im Rahmen des [Young Coders Festival](http://youngcoders.at) entstanden ist.
---
+++ data/exp_Baumkataster.js
@@ -0,0 +1,8 @@
+var exp_Baumkataster = {
+  "type": "FeatureCollection",
+  "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+  "features": [
+  { "type": "Feature", "properties": { "color_qgis2leaf": '#99f17f', "radius_qgis2leaf": 4.0, "transp_qgis2leaf": 1.0, "transp_fill_qgis2leaf": 1.0, "OBJECTID": "19565135", "BAUMNUMMER": "1043  ", "GEBIET": "Strassen", "STRASSE": "Mauerbachstrasse", "ART": "Aesculus hippocastanum (Rosskastanie)", "PFLANZJAHR": "1965", "STAMMUMFANG": "159", "KRONENDURCHMESSER": "5", "BAUMHOEHE": "12", "SE_ANNO_CAD_DATA": null }, "geometry": { "type": "Point", "coordinates": [ 16.220953213322677, 48.215056268264071 ] } },
+  { "type": "Feature", "properties": { "color_qgis2leaf": '#99f17f', "radius_qgis2leaf": 4.0, "transp_qgis2leaf": 1.0, "transp_fill_qgis2leaf": 1.0, "OBJECTID": "19590425", "BAUMNUMMER": "1044  ", "GEBIET": "Strassen", "STRASSE": "Mauerbachstrasse", "ART": "Acer campestre (Feldahorn)", "PFLANZJAHR": "1930", "STAMMUMFANG": "226", "KRONENDURCHMESSER": "9", "BAUMHOEHE": "15", "SE_ANNO_CAD_DATA": null }, "geometry": { "type": "Point", "coordinates": [ 16.220826839329412, 48.215185014315558 ] } }
+  ]
+}
